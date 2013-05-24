80713a0d982d3a456f16e7cd03267ad4ebc2a240
Carlos Aguayo <carlos.aguayo@gmail.com>
2013-05-23 20:59:55 -0400

    insertion sort

---
+++ src/sorting/core.clj
@@ -0,0 +1,28 @@
+(ns sorting.core
+  (:gen-class))
+
+(defn -main
+  [& args]
+  ;; work around dangerous default behaviour in Clojure
+  (alter-var-root #'*read-eval* (constantly false))
+  (println "Hello, World!"))
+
+(defn insert0 
+  "Insert an element into a sorted array keeping the array sorted"
+  [sorted element]
+  (cond
+    (empty? sorted) (list element)
+    (< element (first sorted)) (conj (seq sorted) element)
+    :else (conj (insert0 (rest sorted) element) (first sorted))))
+
+(defn insert1 
+  "Insert all elements of unsorted into sorted keeping the array sorted"
+  [sorted unsorted]
+  (if (empty? unsorted)
+    sorted
+    (insert1 (insert0 sorted (first unsorted)) (rest unsorted))))
+
+(defn insertion-sort 
+  "Sorts the array of elements using the insertion sort algorithm"
+  [elements]
+  (insert1 [] elements))
---
+++ test/sorting/core_test.clj
@@ -0,0 +1,17 @@
+(ns sorting.core-test
+  (:require [clojure.test :refer :all]
+            [sorting.core :refer :all]))
+
+(deftest test-insertion-sort
+ (is (= [] (insertion-sort [])))
+ (is (= [1] (insertion-sort [1])))
+ (is (= [1 2] (insertion-sort [1 2])))
+ (is (= [1 2] (insertion-sort [2 1])))
+ (is (= [1 2 3] (insertion-sort [1 2 3])))
+ (is (= [1 2 3] (insertion-sort [2 1 3])))
+ (is (= [1 2 3] (insertion-sort [2 3 1])))
+ (is (= [1 2 3] (insertion-sort [1 3 2])))
+ (is (= [1 2 3] (insertion-sort [3 1 2])))
+ (is (= [1 2 3] (insertion-sort [3 2 1])))
+ (is (= [1 2 3 4 5] (insertion-sort [1 2 3 4 5])))
+ (is (= [1 2 3 4 5] (insertion-sort [5 4 3 2 1]))))
